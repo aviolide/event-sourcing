11620c5a3bfcbed9ff9eb34b62875a9bb84d59fc
aviolide <desidedsbk17@gmail.com>
2026-05-12 16:26:47 +0800

    feat(saga): add main, app module and env validation

---
+++ 05-saga/src/app.module.ts
@@ -0,0 +1,30 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { envSchema } from './config/env.validation';
+import { SagaModule } from './modules/saga/infrastructure/presentation/saga.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validate: (config) => envSchema.parse(config),
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'postgres',
+        host: config.get<string>('DB_HOST'),
+        port: config.get<number>('DB_PORT'),
+        database: config.get<string>('DB_NAME'),
+        username: config.get<string>('DB_USERNAME'),
+        password: config.get<string>('DB_PASSWORD'),
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
+    }),
+    SagaModule,
+  ],
+})
+export class AppModule {}
