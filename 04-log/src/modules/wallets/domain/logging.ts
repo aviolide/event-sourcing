202fd45c048070f70aa7b781b516dce5fc57511b
aviolide <desidedsbk17@gmail.com>
2026-05-12 14:16:38 +0800

    remove

--- 04-log/src/modules/wallets/domain/logging.ts
+++
@@ -1,8 +0,0 @@
-
-
-export class Logging {
-
-  constructor(props: LoggingProps) {
-  }
-
-}
--- 04-log/src/modules/wallets/domain/repositories/logging.repository.ts
+++
@@ -1,7 +0,0 @@
-import { Result } from 'neverthrow';
-import { Logging } from '../logging';
-import { BaseException } from '../../../../core/exceptions/base.exception';
-
-
-export abstract class LoggingRepository {
-}
